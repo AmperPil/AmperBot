046abc32aee13617998b9e99b9dfd98c137db758
Amperpil <amperpil@gmail.com>
2015-03-13 23:29:33 +0000

    Moved connection info from main file, to a seperate config file. Some grammar was also changed to make better sense.

--- bot_main.js
+++ bot_main.js
@@ -1,5 +1,4 @@
-// Requires
-var irc = require('twitch-irc');
+var config = require('./lib/config');
 var rates = require('./lib/rates');
 //Variables
 var userSpecialString;
@@ -9,29 +8,16 @@ var botAdmins = ['amperpil', 'kz_frew', 'eidgod', 'feurigerilias'];
 //Date and time
 var d = new Date();
 var datetime = "The current date and time is: " + d.getDate() + "/"
-				+ (d.getMonth()+1)  + "/"
-				+ d.getFullYear() + " @ "
-				+ d.getHours() + ":"
-				+ d.getMinutes() + ":"
-				+ d.getSeconds();
+                                + (d.getMonth()+1)  + "/"
+                                + d.getFullYear() + " @ "
+                                + d.getHours() + ":"
+                                + d.getMinutes() + ":"
+                                + d.getSeconds();
 //Connection Info
-var clientOptions = {
-	options: {
-		debug: true,
-		debugIgnore: ['ping', 'chat', 'action']
-	},
-	identity: {
-		username: 'AmperBot',
-		password: ''
-	},
-	channels: ['amperpil', 'kz_frew', 'amperbot', 'eidgod', 'feurigerilias']
-}
-var client = new irc.client(clientOptions);
-var rlimit = new rates(client);
+var rlimit = new rates(config);
+config.client.connect();
 
-client.connect();
-
-client.addListener('chat', function (channel, user, message) {
+config.client.addListener('chat', function (channel, user, message) {
 	//User Levels
 	var twitStaff = user.special.indexOf('staff') > -1;
 	var twitAdmin = user.special.indexOf('admin') > -1;
@@ -61,112 +47,112 @@ client.addListener('chat', function (channel, user, message) {
 			userSpecialString += '.'
 		}
 		// then return/send the message in some way, if this was a function:
-		rlimit.queueCommand(channel, function() { client.say(channel, userSpecialString); }); // print the special string to the channel.
+		rlimit.queueCommand(channel, function() { config.client.say(channel, userSpecialString); }); // print the special string to the channel.
 	}
 	//Commands command
-	if (message.toLowerCase() === '&commands') {
-		rlimit.queueCommand(channel, function() { client.say(channel, 'The commands for this bot can be found at: http://bit.ly/AmperBotHelp'); });
+	else if (message.toLowerCase() === '&commands') {
+		rlimit.queueCommand(channel, function() { config.client.say(channel, 'The commands for this bot can be found at: http://bit.ly/AmperBotHelp'); });
 	}
-	if (message.toLowerCase() === '&github') {
-		rlimit.queueCommand(channel, function() { client.say(channel, 'The github repository for the bot can be found here: https://github.com/AmperPil/AmperBot'); });
+	else if (message.toLowerCase() === '&github') {
+		rlimit.queueCommand(channel, function() { config.client.say(channel, 'The github repository for the bot can be found here: https://github.com/AmperPil/AmperBot'); });
 	}
 	//Join custom channel
-	if (message.toLowerCase().indexOf('&admin_join') === 0) {
+	else if (message.toLowerCase().indexOf('&admin_join') === 0) {
 		if (botAdmins.indexOf(user.username) > -1){
 			//this will make this new string only contain the entries to the command
 			var channelToJoin = message.replace('&join ', '');
-			client.join(channelToJoin);
-			rlimit.queueCommand(channel, function() { client.say(channel, 'The bot has now joined ' + channelToJoin + ' Enjoy!'); });
+			config.client.join(channelToJoin);
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'The bot has now joined ' + channelToJoin + ' Enjoy!'); });
 		} else {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'You do not have the permissions to do this command.'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'You do not have the permissions to do this command.'); });
 		}
 	}
 	//Join user's channel
-	if (message.toLowerCase() === '&join') {
-		client.join(user.username);
-		rlimit.queueCommand(channel, function() { client.say(channel, 'The bot has now joined your channel, Enjoy!'); });
+	else if (message.toLowerCase() === '&join') {
+		config.client.join(user.username);
+		rlimit.queueCommand(channel, function() { config.client.say(channel, 'The bot has now joined your channel, Enjoy!'); });
 	}
 	//leave current channel
-	if (message.toLowerCase() === '&leave') {
+	else if (message.toLowerCase() === '&leave') {
 		if (botAdmins.indexOf(user.username) > -1 || twitBroad) {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'The bot is now going to leave your channel.'); });
-			client.part(channel);
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'The bot is now going to leave your channel.'); });
+			config.client.part(channel);
 		} else if (botAdmins.indexOf(user.username) === -1) {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'You do not have the permissions to do this command.'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'You do not have the permissions to do this command.'); });
 		} else {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Something went wrong, sorry! <3'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Something went wrong, sorry! <3'); });
 		}
 	}
 	//Hug command
-	if (message.toLowerCase().indexOf('&hug') === 0) {
+	else if (message.toLowerCase().indexOf('&hug') === 0) {
 		if (user.special.indexOf('mod') >= 0 || botAdmins.indexOf(user.username) > -1){
 			var hugRecipent = message.replace('&hug ', '');
-			rlimit.queueCommand(channel, function() { client.say(channel, '/me Gives ' + hugRecipent + ' a big hug!'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, '/me gives ' + hugRecipent + ' a big hug!'); });
 		} else {
 			return;
 		}
 	}
 	//kill command
-	if (message.toLowerCase().indexOf('&kill') === 0) {
+	else if (message.toLowerCase().indexOf('&kill') === 0) {
 		if (user.special.indexOf('mod') >= 0 || botAdmins.indexOf(user.username) > -1){
 			var killRecipent = message.replace('&kill ', '');
-			rlimit.queueCommand(channel, function() { client.say(channel, '/me Stabs ' + killRecipent + ' in the chest, killing them. RIP In Peace.'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, '/me stabs ' + killRecipent + ' in the chest, killing them. RIP In Peace.'); });
 		} else {
 			return;
 		}
 	}
 	//Pyramid command
-	if (message.toLowerCase() === '&pyramid') {
+	else if (message.toLowerCase() === '&pyramid') {
 		if (user.special.indexOf('mod') >= 0 || botAdmins.indexOf(user.username) > -1)
-		rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa'); });
+		rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa'); });
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa Kappa'); });
 		}, 1000);
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa Kappa Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa Kappa Kappa'); });
 		}, 2000);
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa Kappa Kappa Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa Kappa Kappa Kappa'); });
 		}, 3000);
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa Kappa Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa Kappa Kappa'); });
 		}, 4000);
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa Kappa'); });
 		}, 5000);
 		setTimeout(function(){
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Kappa'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Kappa'); });
 		}, 6000);
 	}
 	//Time Command
-	if (message.toLowerCase() === '&time') {
-		rlimit.queueCommand(channel, function() { client.say(channel, datetime + ' (GMT 0)'); });
+	else if (message.toLowerCase() === '&time') {
+		rlimit.queueCommand(channel, function() { config.client.say(channel, datetime + ' (GMT 0)'); });
 	}
 	//Ability to add admins to the bot through the chat.
-	if (message.toLowerCase().indexOf('&admin_add') === 0) {
+	else if (message.toLowerCase().indexOf('&admin_add') === 0) {
 		if (botAdmins.indexOf(user.username) > -1) {
 			var adminRecipent = message.replace('&admin_add ', '');
 			botAdmins.push(adminRecipent);
-			rlimit.queueCommand(channel, function() { client.say(channel, adminRecipent + ' has been added as an admin of the bot.'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, adminRecipent + ' has been added as an admin of the bot.'); });
 		} else if (botAdmins.indexOf(user.username) === -1) {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'You do not have the permission to do this command.'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'You do not have the permission to do this command.'); });
 		} else {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Something went wrong, sorry! <3'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Something went wrong, sorry! <3'); });
 		}
 	}
 	//Check if user is in the admin list
-	if (message.toLowerCase() === '&admin_check') {
+	else if (message.toLowerCase() === '&admin_check') {
 		if (botAdmins.indexOf(user.username) > -1) {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Congratulations! You are one of the admins! <3'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Congratulations! You are one of the admins! <3'); });
 		} else if (botAdmins.indexOf(user.username) === -1) {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Sorry, You are not one of the admins :('); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Sorry, You are not one of the admins :('); });
 		} else {
-			rlimit.queueCommand(channel, function() { client.say(channel, 'Something went wrong, sorry! <3'); });
+			rlimit.queueCommand(channel, function() { config.client.say(channel, 'Something went wrong, sorry! <3'); });
 		}
 	}
 	//Prints out the list of admins for the bot.
-	if (message.toLowerCase() === '&admin_list') {
-		rlimit.queueCommand(channel, function() { client.say(channel, 'The admins for the bot are: ' + botAdmins); });
+	else if (message.toLowerCase() === '&admin_list') {
+		rlimit.queueCommand(channel, function() { config.client.say(channel, 'The admins for the bot are: ' + botAdmins); });
 	}
 	//Random Colour(Sorry S)
 	var randomNum = Math.floor((Math.random() * 14) + 1);
@@ -202,7 +188,7 @@ client.addListener('chat', function (channel, user, message) {
 		chatCol = 'YellowGreen';
 	}
 	if (message.toLowerCase().indexOf('&') === 0) {
-		client.color(channel, chatCol);
+		config.client.say(channel, '/color ' + chatCol);
 		console.log('Changed colour to:' + chatCol + ' in ' + channel);
 	}
 });
